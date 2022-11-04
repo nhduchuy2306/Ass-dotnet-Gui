8ee80a7806e4a1c4ede4697bd107d45459709e99
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2022-11-04 23:40:42 +0700

    second commit

--- DataAccess/Repository/OrderRepository.cs
+++ DataAccess/Repository/OrderRepository.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Repository
+namespace DataAccess.Repository;
+
+public class OrderRepository
 {
-    public class OrderRepository
-    {
-    }
 }
