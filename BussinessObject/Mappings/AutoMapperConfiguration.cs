1762858d428f7d26e7e7f79fa121b0f46a70663f
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2022-11-06 15:50:24 +0700

    modify product

---
+++ BussinessObject/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObject.Mappings;
+
+public class AutoMapperConfiguration
+{
+    public static Product ToProduct(ProductObject productObject)
+    {
+        var config = new MapperConfiguration(cfg => 
+            cfg.CreateMap<ProductObject, Product>()
+        );
+
+        var mapper = new Mapper(config);
+        var product = mapper.Map<Product>(productObject);
+
+        return product;
+    }
+}
--- BussinessObject/ProductObject.cs
+++ BussinessObject/ProductObject.cs
@@ -8,5 +8,10 @@ namespace BussinessObject;
 
 public class ProductObject
 {
-
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string Weight { get; set; } = null!;
+    public decimal UnitPrice { get; set; }
+    public int UnitsInStock { get; set; }
 }
