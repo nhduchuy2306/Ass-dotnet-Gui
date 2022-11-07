2eab8448610c5eb33d6db6a6eeeeaa54cb847739
Vuong Huu Hieu <ownhieu0804@gmail.com>
2022-11-07 17:18:03 +0700

    update src

--- DataAccess/Repository/MemberRepository.cs
+++ DataAccess/Repository/MemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BussinessObject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,19 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repository;
 
-public class MemberRepository
+public class MemberRepository : IMemberRepository
 {
+    private SaleManagementContext _saleManagementContext;
+    public MemberRepository(SaleManagementContext saleManagementContext)
+    {
+        _saleManagementContext = saleManagementContext;
+    }
+    public MemberRepository()
+    {
+        _saleManagementContext = new SaleManagementContext();
+    }
+
+    public List<Member> GetMembers() => _saleManagementContext.Members.ToList();
+    public string GetEmailAdmin() => _saleManagementContext.GetEmailAdmin();
+    public string GetPassAdmin() => _saleManagementContext.GetPassAdmin();
 }
