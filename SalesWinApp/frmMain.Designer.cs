fa5d39ff6b4b7ecdaca6e65ead6bd0b6cfa47191
n-d-h <duchoang4195@gmail.com>
2022-11-08 13:06:00 +0700

    fix

--- SalesWinApp/frmMain.Designer.cs
+++ SalesWinApp/frmMain.Designer.cs
@@ -41,69 +41,76 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.memberManagementToolStripMenuItem,
             this.productManagementToolStripMenuItem,
             this.orderManagementToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 35);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1117, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1596, 35);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // memberManagementToolStripMenuItem
             // 
             this.memberManagementToolStripMenuItem.Name = "memberManagementToolStripMenuItem";
-            this.memberManagementToolStripMenuItem.Size = new System.Drawing.Size(138, 20);
-            this.memberManagementToolStripMenuItem.Text = "Member Management";            // 
+            this.memberManagementToolStripMenuItem.Size = new System.Drawing.Size(205, 29);
+            this.memberManagementToolStripMenuItem.Text = "Member Management";
+            this.memberManagementToolStripMenuItem.Click += new System.EventHandler(this.memberManagementToolStripMenuItem_Click);
+            // 
             // productManagementToolStripMenuItem
             // 
             this.productManagementToolStripMenuItem.CheckOnClick = true;
             this.productManagementToolStripMenuItem.Name = "productManagementToolStripMenuItem";
-            this.productManagementToolStripMenuItem.Size = new System.Drawing.Size(135, 20);
+            this.productManagementToolStripMenuItem.Size = new System.Drawing.Size(200, 29);
             this.productManagementToolStripMenuItem.Text = "Product Management";
             this.productManagementToolStripMenuItem.Click += new System.EventHandler(this.productManagementToolStripMenuItem_Click);
             // 
             // orderManagementToolStripMenuItem
             // 
             this.orderManagementToolStripMenuItem.Name = "orderManagementToolStripMenuItem";
-            this.orderManagementToolStripMenuItem.Size = new System.Drawing.Size(123, 20);
+            this.orderManagementToolStripMenuItem.Size = new System.Drawing.Size(184, 29);
             this.orderManagementToolStripMenuItem.Text = "Order Management";
             this.orderManagementToolStripMenuItem.Click += new System.EventHandler(this.orderManagementToolStripMenuItem_Click);
             // 
             // menuStrip2
             // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.profileToolStripMenuItem,
             this.historyOrderToolStripMenuItem});
             this.menuStrip2.Location = new System.Drawing.Point(0, 0);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(1117, 24);
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip2.Size = new System.Drawing.Size(1596, 35);
             this.menuStrip2.TabIndex = 2;
             this.menuStrip2.Text = "menuStrip2";
             // 
             // profileToolStripMenuItem
             // 
             this.profileToolStripMenuItem.Name = "profileToolStripMenuItem";
-            this.profileToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.profileToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
             this.profileToolStripMenuItem.Text = "Profile";
             this.profileToolStripMenuItem.Click += new System.EventHandler(this.profileToolStripMenuItem_Click);
             // 
             // historyOrderToolStripMenuItem
             // 
             this.historyOrderToolStripMenuItem.Name = "historyOrderToolStripMenuItem";
-            this.historyOrderToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.historyOrderToolStripMenuItem.Size = new System.Drawing.Size(136, 29);
             this.historyOrderToolStripMenuItem.Text = "History Order";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1117, 589);
+            this.ClientSize = new System.Drawing.Size(1596, 982);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.menuStrip2);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmMain";
