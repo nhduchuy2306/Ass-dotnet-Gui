90bb36f8b41d1669de9c630384033ef41df44e3f
Vuong Huu Hieu <ownhieu0804@gmail.com>
2022-11-18 12:21:19 +0700

    update logout

--- SalesWinApp/frmLogin.Designer.cs
+++ SalesWinApp/frmLogin.Designer.cs
@@ -44,9 +44,9 @@
             // 
             this.lbEmail.AutoSize = true;
             this.lbEmail.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbEmail.Location = new System.Drawing.Point(75, 100);
+            this.lbEmail.Location = new System.Drawing.Point(86, 133);
             this.lbEmail.Name = "lbEmail";
-            this.lbEmail.Size = new System.Drawing.Size(58, 25);
+            this.lbEmail.Size = new System.Drawing.Size(71, 32);
             this.lbEmail.TabIndex = 0;
             this.lbEmail.Text = "Email";
             // 
@@ -54,9 +54,9 @@
             // 
             this.lbPassword.AutoSize = true;
             this.lbPassword.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbPassword.Location = new System.Drawing.Point(75, 160);
+            this.lbPassword.Location = new System.Drawing.Point(86, 213);
             this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(91, 25);
+            this.lbPassword.Size = new System.Drawing.Size(111, 32);
             this.lbPassword.TabIndex = 1;
             this.lbPassword.Text = "Password";
             // 
@@ -65,9 +65,10 @@
             this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnLogin.FlatAppearance.BorderSize = 0;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Location = new System.Drawing.Point(136, 226);
+            this.btnLogin.Location = new System.Drawing.Point(155, 301);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(93, 28);
+            this.btnLogin.Size = new System.Drawing.Size(106, 37);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -80,9 +81,10 @@
             this.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnExit.FlatAppearance.BorderSize = 0;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Location = new System.Drawing.Point(298, 226);
+            this.btnExit.Location = new System.Drawing.Point(341, 301);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(93, 28);
+            this.btnExit.Size = new System.Drawing.Size(106, 37);
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -95,22 +97,24 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 269);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 366);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(539, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(616, 22);
             this.statusStrip1.TabIndex = 4;
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 16);
             // 
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtEmail.Location = new System.Drawing.Point(216, 98);
+            this.txtEmail.Location = new System.Drawing.Point(247, 131);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(254, 32);
+            this.txtEmail.Size = new System.Drawing.Size(290, 39);
             this.txtEmail.TabIndex = 5;
             this.txtEmail.MouseLeave += new System.EventHandler(this.txtEmail_MouseLeave);
             this.txtEmail.MouseHover += new System.EventHandler(this.txtEmail_MouseHover);
@@ -118,10 +122,11 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtPassword.Location = new System.Drawing.Point(216, 158);
+            this.txtPassword.Location = new System.Drawing.Point(247, 211);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(254, 32);
+            this.txtPassword.Size = new System.Drawing.Size(290, 39);
             this.txtPassword.TabIndex = 6;
             this.txtPassword.MouseLeave += new System.EventHandler(this.txtPassword_MouseLeave);
             this.txtPassword.MouseHover += new System.EventHandler(this.txtPassword_MouseHover);
@@ -130,17 +135,17 @@
             // 
             this.lbLogin.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lbLogin.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lbLogin.Location = new System.Drawing.Point(171, 30);
+            this.lbLogin.Location = new System.Drawing.Point(195, 40);
             this.lbLogin.Name = "lbLogin";
-            this.lbLogin.Size = new System.Drawing.Size(210, 34);
+            this.lbLogin.Size = new System.Drawing.Size(240, 45);
             this.lbLogin.TabIndex = 7;
             this.lbLogin.Text = "FSTORE LOGIN";
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 291);
+            this.ClientSize = new System.Drawing.Size(616, 388);
             this.Controls.Add(this.lbLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtEmail);
@@ -149,9 +154,11 @@
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.lbEmail);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LOGIN ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
             this.Load += new System.EventHandler(this.frmLogin_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
